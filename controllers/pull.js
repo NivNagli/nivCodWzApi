60d33ea87c0568ad03d81993b371c12a5dba0547
Niv Nagli <itsniv@gmail.com>
2021-09-17 18:03:19 +0300

    doc fix

--- controllers/pull.js
+++ controllers/pull.js
@@ -15,7 +15,7 @@ const atkn = settings.ATKN;
  * 
  * Note:
  * I do not reveal the file 'setting.js' because it contains personal information about my user,
- * The titles appear here at the beginning of the code that you need to extract from the session in order to use the code.
+ * The objects appear here at the beginning of the code [ACT_SSO_COOKIE, ACT_SSO_COOKIE_EXPIRY, ATKN] is what you need to extract from the session in order to use the code.
  ************************************************************************************************************************************************************************/
 
 /* ================================================== Proteced methods [need session cookies!] ======================================================================================= */
